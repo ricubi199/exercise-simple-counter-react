37f84aed6314bc0cc254f99a88a2d6ffeb3dc200
ricubi199 <cubilloricardo@outlook.es>
2021-03-07 15:54:10 +0000

    Add more digit

--- src/js/component/seconds-counter.js
+++ src/js/component/seconds-counter.js
@@ -5,18 +5,35 @@ export function SecondsCounter(props) {
 	return (
 		<div className="seconds-counter">
 			<div className="clock-icon">
-				<span class="badge text-white">
+				<span className="badge text-white badge-dark">
 					<i className="fas fa-clock"></i>
 				</span>
 			</div>
+			<div className="second5">
+				<span className="badge text-white badge-dark">
+					{props.digit5 % 10}
+				</span>
+			</div>
+			<div className="second4">
+				<span className="badge text-white badge-dark">
+					{props.digit4 % 10}
+				</span>
+			</div>
+			<div className="second3">
+				<span className="badge text-white badge-dark">
+					{props.digit3 % 10}
+				</span>
+			</div>
 			<div className="second2">
-				<span class="badge text-white">{props.digit2 % 10}</span>
+				<span className="badge text-white badge-dark">
+					{props.digit2 % 10}
+				</span>
 			</div>
 			<div className="second1">
-				<span class="badge text-white">{props.digit1 % 10}</span>
+				<span className="badge badge-dark">{props.digit1 % 10}</span>
 			</div>
 			<div className="second0">
-				<span class="badge text-white">{props.digit0 % 10}</span>
+				<span className="badge text-white">{props.digit0 % 10}</span>
 			</div>
 		</div>
 	);
@@ -25,5 +42,8 @@ export function SecondsCounter(props) {
 SecondsCounter.propTypes = {
 	digit0: PropTypes.number,
 	digit1: PropTypes.number,
-	digit2: PropTypes.number
+	digit2: PropTypes.number,
+	digit3: PropTypes.number,
+	digit4: PropTypes.number,
+	digit5: PropTypes.number
 };
